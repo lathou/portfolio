ffc3aface5339a33d030dac5f161ca233d953e61
Nath <tholfou@gmail.com>
2016-01-05 14:54:24 +0100

    n'envoie que si les champs sont remplis

--- app/js/ajax-mail.js
+++ app/js/ajax-mail.js
@@ -1,37 +1,39 @@
 var envoyer = document.getElementById('sendMail');
 var form = document.querySelector('form');
 
-envoyer.addEventListener('click',function(e){
-	e.preventDefault();
-})
-
 envoyer.addEventListener('click', function(){
+	e.preventDefault();
 	var nom = document.getElementById('nom').value,
 		prenom = document.getElementById('prenom').value,
 		mail = document.getElementById('mail').value,
 		message = document.getElementById('message').value;
 
-	message = nom + ' ' + prenom + ' :' + mail + '\n\n' + message;
+	if(nom && prenom && mail && message){
 
-	var xhr = new XMLHttpRequest();
-	xhr.open('POST', 'http://sophietk.herokuapp.com/mail', true);
-	xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-	xhr.send(JSON.stringify({
-		from: "sender@hihi.com",
-		to: "tholfou@gmail.com",
-		subject: "Portfolio",
-		text: message
-	}));
+		message = nom + ' ' + prenom + ' :' + mail + '\n\n' + message;
 
-	xhr.addEventListener('readystatechange', function(){
-		if(xhr.readyState === 4){
-			if(xhr.status === 200){
-				alert('Votre message a bien été envoyé');
-				form.reset();	
-			}else{
-				alert('Une erreur est survenue, votre message n\'a pas pu être envoyé');
+		var xhr = new XMLHttpRequest();
+		xhr.open('POST', 'http://sophietk.herokuapp.com/mail', true);
+		xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+		xhr.send(JSON.stringify({
+			from: "sender@hihi.com",
+			to: "tholfou@gmail.com",
+			subject: "Portfolio",
+			text: message
+		}));
+
+		xhr.addEventListener('readystatechange', function(){
+			if(xhr.readyState === 4){
+				if(xhr.status === 200){
+					alert('Votre message a bien été envoyé');
+					form.reset();	
+				}else{
+					alert('Une erreur est survenue, votre message n\'a pas pu être envoyé');
+				}
 			}
-		}
-	},false);
+		},false);
+	}else{
+		alert('Veuillez remplir les champs');
+	}
 }, false);
 
